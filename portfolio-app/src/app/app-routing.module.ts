f8311932368c0b28e9acbf6f55db436597d4e07a
amaani <amaanzy@gmail.com>
2021-08-25 11:37:03 -0400

    added components

--- portfolio-app/src/app/app-routing.module.ts
+++ portfolio-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CashValueComponent } from './cash-value/cash-value.component';
+import { InvestmentsComponent } from './investments/investments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "cash-value", component: CashValueComponent},
+  {path: "investments", component: InvestmentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { CashValueComponent } from './cash-value/cash-value.component';
+import { NetWorthComponent } from './net-worth/net-worth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentsComponent,
+    CashValueComponent,
+    NetWorthComponent
   ],
   imports: [
     BrowserModule,
