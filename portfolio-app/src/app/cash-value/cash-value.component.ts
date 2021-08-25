3b15d1badada2b258c890c0a7a1ec1423b04de33
aleezah <aleeza97@hotmail.com>
2021-08-25 19:01:05 -0400

    fixed the values for cashAccount

--- portfolio-app/src/app/cash-value/cash-value.component.ts
+++ portfolio-app/src/app/cash-value/cash-value.component.ts
@@ -10,15 +10,24 @@ import { PortfolioService } from '../portfolio.service';
 export class CashValueComponent implements OnInit {
 
   cashAcct: any = []
+  acctNames: string[] = [];
   submitClicked = true
   backClicked = false
+  accountFunds=0
+  accountName=""
+  accountId=0
+  accountType=""
+  acct = this.cashAcct[0]
+  
 
   constructor(private portfolioService: PortfolioService) {
-    this.cashAcct=this.getTotalAccounts()
+    
+
+   
  }
    
   form = new FormGroup({
-    option: new FormControl('', Validators.required)
+    option: new FormControl('')
   });
    
   get f(){
@@ -26,9 +35,31 @@ export class CashValueComponent implements OnInit {
   }
    
   submit(){
+    console.log(this.form.value.option)
+    if(this.form.value === '')
+    {
+
+    }
+    else{
     this.submitClicked = false
     this.backClicked = true
-    console.log(this.cashAcct)
+    console.log(this.form.value.option)
+
+    
+    
+    for(let account of this.cashAcct){
+     // console.log(account.cashaccname)
+      if (this.form.value.option===account.cashaccname)
+      {
+        this.acct=account
+        this.accountFunds=account.funds 
+        this.accountName=account.cashaccname
+        this.accountType=account.accounttype
+        this.accountId=account.cashaccountid
+      }
+      console.log(account)
+    }
+  }
   }
 
   back(){
@@ -37,6 +68,7 @@ export class CashValueComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.cashAcct=this.getTotalAccounts()
   }
   getTotalAccounts(){
     //TODO: add the actual service call once the api is set up
@@ -44,8 +76,24 @@ export class CashValueComponent implements OnInit {
      this.portfolioService.getCashAccounts().subscribe((cashAccounts)=>{
 
       this.cashAcct = cashAccounts
+      
+      this.acctNames=this.getCashAccountNames()
+      
      
   }
-     )
+  
+     ) 
   }
+  getCashAccountNames(){
+    this.acctNames = []
+    console.log("getting account names")
+    console.log(this.cashAcct)
+    for(let account of this.cashAcct){
+      console.log(account)
+      this.acctNames.push(account.cashaccname)
+      console.log("name"+account.cashaccname)
+    }
+    return this.acctNames;
+  }
+  
 }
