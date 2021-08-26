5b7c363207940321518bed39777a1c66cc3d6ec7
aleezah <aleeza97@hotmail.com>
2021-08-26 12:39:20 -0400

    Started Net Worth

--- portfolio-app/src/app/net-worth/net-worth.component.ts
+++ portfolio-app/src/app/net-worth/net-worth.component.ts
@@ -1,4 +1,7 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { PortfolioService } from '../portfolio.service';
 
 @Component({
   selector: 'app-net-worth',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetWorthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private portfolioService: PortfolioService) {
+    this.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    this.data = [45, 55, 35, 65, 60, 25, 45];
+  }
+  data = [] as any;
+  labels = [] as any;
+  netWorthSum=0
+  cashTotal=0
+  investTotal=0
+  cashAcct: any = []
+  investAcct: any = []
 
   ngOnInit(): void {
+    this.cashAcct=this.getTotalAccounts()
+    this.investAcct=this.getInvestmentAccounts()
   }
 
+  getTotalAccounts()
+  {
+    this.portfolioService.getCashAccounts().subscribe((cashAccounts)=>{
+    this.cashAcct = cashAccounts
+    this.getCashTotal()
+
+    }
+    )
+
+  } 
+  getInvestmentAccounts(){
+
+    this.portfolioService.getInvestments().subscribe((investmentAccounts)=>{
+      console.log(investmentAccounts);
+      this.investAcct = investmentAccounts;
+
+      //calculate the total investments
+      this.getInvestTotal();
+    })
+    
+  }
+
+  getCashTotal(){
+    console.log("here")
+  for (let account of this.cashAcct )
+  {
+    this.cashTotal=account.funds+this.cashTotal
+  }
+  return this.cashTotal
+
+}
+getNetWorth(){
+this.netWorthSum=this.cashTotal+this.investTotal
+return this.netWorthSum
+}
+
+getInvestTotal(){
+  console.log("here")
+for (let account of this.investAcct )
+{
+  console.log(account)
+  this.investTotal=account.funds+this.investTotal
+}
+return this.investTotal
+
+}
+  
+  
 }
